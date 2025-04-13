bd51988a90c4779939da5217de2ce660ac159726
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2025-04-13 14:44:13 -0700

    Make selected option bluer

--- frontend/src/components/Question.tsx
+++ frontend/src/components/Question.tsx
@@ -25,7 +25,7 @@ function Question({
             className={`block w-64 px-4 py-2 border rounded cursor-pointer text-center transition
               ${
                 selectedOption === option
-                  ? "bg-blue-100 border-blue-500"
+                  ? "bg-blue-300 border-blue-500"
                   : "bg-white border-gray-300"
               }
               hover:bg-blue-50`}
